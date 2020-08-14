0775c39873655c3daf7707e863c43e97520fbe4c
Oleksandr Bukovskyi <oleksandrbu@tahometer.com>
2020-08-14 11:02:30 +0300

    local

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace First_Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls("https://0.0.0.0:3031", "http://0.0.0.0:3030");
+                    webBuilder.UseStartup<Startup>(); 
+                });
+    }
+}
